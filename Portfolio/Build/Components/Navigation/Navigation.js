6835caa384af0eb987945e608de673380f925b03
justkeepdoingit <95115805+justkeepdoingit@users.noreply.github.com>
2023-05-05 18:01:00 +0800

    Updated website

--- Portfolio/Build/Components/Navigation/Navigation.js
+++ Portfolio/Build/Components/Navigation/Navigation.js
@@ -1,21 +1,51 @@
-export const Button = ({ content, type, className = "" }) => {
-  return createEl("a", { class: `btn ${type} ${className}` }, content);
+export const Button = ({
+  content,
+  type = "",
+  className = "",
+  eventListener = {},
+}) => {
+  return createEl(
+    "a",
+    { class: `btn ${type} ${className}` },
+    content,
+    eventListener
+  );
 };
+
+let active = false;
 
 export const Navigation = ({ Logo, Nav = [], CTA = [] }) => {
   let navs = Nav.map((data) => createEl("a", { class: "cta-nav" }, data));
+  let mobile = screen.width < 768 ? `mobile ${active ? "active" : ""}` : "";
 
-  return Container(
-    "div",
-    "navigation-row",
+  return Container("div", `navigation-row ${mobile}`, [
     Container("div", "navigation-container", [
-      createEl("div", { class: "logo" }, Logo),
+      createEl("div", { class: `logo ${mobile}` }, Logo),
       createEl("div", { class: "navigation" }, navs),
-      createEl(
-        "div",
-        { class: "cta-container" },
-        Button({ content: CTA[0], type: "primary" })
-      ),
-    ])
-  );
+      createEl("div", { class: "cta-container" }, [
+        Button({ content: CTA[0], type: "primary nav" }),
+        Button({
+          content: createEl(
+            "span",
+            { class: "material-symbols-rounded" },
+            `${active ? "close" : "menu"}`
+          ),
+          className: "nav-btn",
+          eventListener: {
+            click: (e) => {
+              let classList = [
+                ".navigation-row.mobile",
+                ".nav-btn",
+                ".cta-container",
+              ];
+              for (let i = 0; i < classList.length; i++) {
+                document.querySelector(classList[i]).classList.add("active");
+              }
+              document.querySelector(".logo").classList.remove("mobile");
+            },
+          },
+        }),
+      ]),
+    ]),
+  ]);
 };
